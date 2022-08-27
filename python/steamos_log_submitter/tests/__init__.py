999bccee5a427573e7bc7617dd23591fab47c304
Vicki Pfau <vi@endrift.com>
2022-08-26 18:11:35 -0700

    Add tests for get_appid

--- python/steamos_log_submitter/tests/__init__.py
+++ python/steamos_log_submitter/tests/__init__.py
@@ -4,3 +4,12 @@ def open_shim(text):
     def open_fake(*args):
         return io.StringIO(text)
     return open_fake
+
+
+def open_shim_cb(cb):
+    def open_fake(fname, *args):
+        text = cb(fname)
+        if text is None:
+            raise FileNotFoundError
+        return io.StringIO(text)
+    return open_fake
---
+++ python/steamos_log_submitter/tests/test_get_appid.py
@@ -0,0 +1,95 @@
+import builtins
+import steamos_log_submitter as sls
+from steamos_log_submitter.tests import open_shim_cb
+
+def build_proc_chain(procs):
+    def cb(fname):
+        if not fname.startswith('/proc/'):
+            return None
+        pid, fname = fname.split('/', 3)[2:]
+        pid = int(pid)
+        if pid not in procs:
+            return None
+        ppid, cmdline = procs[pid]
+        if fname == 'stat':
+            return  f'{pid} ({cmdline[0]}) S {ppid} 0'
+        if fname == 'cmdline':
+            return '\0'.join(cmdline)
+        if fname == 'comm':
+            return cmdline[0]
+    return open_shim_cb(cb)
+
+
+def test_dead_pid(monkeypatch):
+    def cb(fname):
+        return None
+    monkeypatch.setattr(builtins, "open", open_shim_cb(cb))
+    assert sls.get_appid(2) is None
+
+
+def test_no_reaper(monkeypatch):
+    procs = {
+        2:  (1, ['tester'])
+    }
+    monkeypatch.setattr(builtins, "open", build_proc_chain(procs))
+    assert sls.get_appid(2) is None
+
+
+def test_reaper(monkeypatch):
+    procs = {
+        2:  (1, ['reaper', 'SteamLaunch', 'AppId=100'])
+    }
+    monkeypatch.setattr(builtins, "open", build_proc_chain(procs))
+    assert sls.get_appid(2) == 100
+
+
+def test_no_steamlaunch(monkeypatch):
+    procs = {
+        2:  (1, ['reaper', 'AppId=100'])
+    }
+    monkeypatch.setattr(builtins, "open", build_proc_chain(procs))
+    assert sls.get_appid(2) is None
+
+
+def test_no_appid(monkeypatch):
+    procs = {
+        2:  (1, ['reaper', 'SteamLaunch'])
+    }
+    monkeypatch.setattr(builtins, "open", build_proc_chain(procs))
+    assert sls.get_appid(2) is None
+
+
+def test_stop_parsing(monkeypatch):
+    procs = {
+        2:  (1, ['reaper', 'SteamLaunch', '--', 'AppId=100'])
+    }
+    monkeypatch.setattr(builtins, "open", build_proc_chain(procs))
+    assert sls.get_appid(2) is None
+
+
+def test_parent(monkeypatch):
+    procs = {
+        2:  (1, ['reaper', 'SteamLaunch', 'AppId=100']),
+        3:  (2, ['tester'])
+    }
+    monkeypatch.setattr(builtins, "open", build_proc_chain(procs))
+    assert sls.get_appid(3) == 100
+
+
+def test_reaper_parent(monkeypatch):
+    procs = {
+        2:  (1, ['reaper', 'SteamLaunch', 'AppId=100']),
+        3:  (2, ['reaper'])
+    }
+    monkeypatch.setattr(builtins, "open", build_proc_chain(procs))
+    assert sls.get_appid(3) == 100
+
+
+def test_parent_parent(monkeypatch):
+    procs = {
+        2:  (1, ['reaper', 'SteamLaunch', 'AppId=100']),
+        3:  (2, ['tester']),
+        4:  (3, ['tester'])
+    }
+    monkeypatch.setattr(builtins, "open", build_proc_chain(procs))
+    assert sls.get_appid(4) == 100
