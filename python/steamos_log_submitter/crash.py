bb5bf40c48e68680a8f5721e18a05bf033909dc6
Vicki Pfau <vi@endrift.com>
2022-09-01 18:00:27 -0700

    Untested kdump helper

---
+++ python/steamos_log_submitter/crash.py
@@ -0,0 +1,41 @@
+import os
+import requests
+import steamos_log_submitter as sls
+
+start_url = "https://api.steampowered.com/ICrashReportService/StartCrashUpload/v1"
+finish_url = "https://api.steampowered.com/ICrashReportService/FinishCrashUpload/v1"
+
+def upload(*, product, build=None, version, info, dump=None, filename=None) -> bool:
+    account = sls.util.get_steam_account_id()
+
+    info = dict(info)
+    info.update({
+        'steam_id': account or 'null',
+        'have_dump_file': 1 if dump else 0,
+        'product': product,
+        'build': build or 'null',
+        'version': version,
+        'platform': 'linux',
+        'format': 'json'
+    })
+    if dump:
+        info['dump_file_size'] = os.stat(dump).st_size
+
+    start = requests.post(start_url, data=info)
+    if start.status // 100 != 2:
+        return False
+
+    response = start.json()['response']
+    headers = {pair.name: pair.value for pair in response['headers']['pairs']}
+    if not filename:
+        filename = os.path.basename(dump)
+
+    put = requests.put(response['url'], headers=headers, files={filename: open(dump, 'rb')})
+    if put.status // 100 != 2:
+        return False
+
+    finish = requests.post(finish_url, data={'gid': response['gid']})
+    if finish.status // 100 != 2:
+        return False
+
+    return True
