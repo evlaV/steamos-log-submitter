b0878cdf2e659921c8a6560cdb77e167e7e63583
Vicki Pfau <vi@endrift.com>
2022-09-20 23:19:04 -0700

    Refactor out crash testing tools

--- tests/helpers/test_kdump.py
+++ tests/helpers/test_kdump.py
@@ -1,8 +1,7 @@
-import json
 import os
-import requests
 import steamos_log_submitter.crash as crash
 import steamos_log_submitter.helpers.kdump as kdump
+from .crash import FakeResponse
 
 file_base = f'{os.path.dirname(__file__)}/kdump'
 
@@ -22,43 +21,10 @@ def test_submit_bad_name():
 
 
 def test_submit_succeed(monkeypatch):
-    attempt = 0
-    def fake_response(body):
-        def ret(url, data=None, *args, **kwargs):
-            nonlocal attempt
-            attempt += 1
-            if attempt == 1:
-                r = requests.Response()
-                r.status_code = 200
-                r._content = body.encode()
-                return r
-            if attempt == 2:
-                assert url == json.loads(body)['response']['url']
-                assert data is not None
-                assert data.read
-                r = requests.Response()
-                r.status_code = 204
-                return r
-            if attempt == 3:
-                assert data and data.get('gid') == 111
-                r = requests.Response()
-                r.status_code = 204
-                return r
-            assert False
-        return ret
-
-    response = json.dumps({'response': {
-        'headers': {
-            'pairs': []
-        },
-        'url': 'file:///',
-        'gid': 111
-    }})
-    respond = fake_response(response)
-    monkeypatch.setattr(requests, 'post', respond)
-    monkeypatch.setattr(requests, 'put', respond)
+    response = FakeResponse()
+    response.success(monkeypatch)
     assert kdump.submit(f'{file_base}/dmesg.zip')
-    assert attempt == 3
+    assert response.attempt == 3
 
 
 def test_submit_empty(monkeypatch):
