b427ec7827e3cbe9c78549a9aa46a9adeb747f11
Vicki Pfau <vi@endrift.com>
2022-09-28 19:35:09 -0700

    Add header and footer

--- steamos_log_submitter/logging.py
+++ steamos_log_submitter/logging.py
@@ -1,3 +1,7 @@
+# SPDX-License-Identifier: LGPL-2.1+
+#
+# Copyright (c) 2022 Valve Software
+# Maintainer: Vicki Pfau <vi@endrift.com>
 import logging
 import steamos_log_submitter as sls
 
@@ -22,3 +26,5 @@ def reconfigure_logging():
         kwargs['filename'] = path
 
     logging.basicConfig(**kwargs)
+
+# vim:ts=4:sw=4:et
