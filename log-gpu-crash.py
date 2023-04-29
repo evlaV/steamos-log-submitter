9b4b47e801799639024f05d5f94450dac400c063
Vicki Pfau <vi@endrift.com>
2023-04-28 17:17:35 -0700

    Install GPU stuff, add devcoredump rule

--- log-gpu-crash.py
+++ log-gpu-crash.py
@@ -4,24 +4,31 @@
 #
 # Copyright (c) 2022 Valve Software
 # Maintainer: Vicki Pfau <vi@endrift.com>
+import logging
 import os
 import time
 import steamos_log_submitter as sls
 
-os.makedirs(f'{sls.pending}/gpu', mode=0o755, exist_ok=True)
-ts = time.time_ns()
+logging.basicConfig(filename=f'{sls.base}/gpu-crash.log', encoding='utf-8', level=logging.INFO, force=True)
+logger = logging.getLogger(__name__)
 
-with open(f'{sls.pending}/gpu/{ts}.log', 'w') as f:
-    pid = None
-    for key, val in os.environ.items():
-        if key in ('PWD', '_'):
-            continue
-        if key == 'PID':
-            pid = int(val)
-        print(f'{key}={val}', file=f)
-    if pid:
-        appid = sls.util.get_appid(pid)
-        print(f'APPID={appid}', file=f)
-    print(f'TIMESTAMP={ts}', file=f)
+try:
+    os.makedirs(f'{sls.pending}/gpu', mode=0o755, exist_ok=True)
+    ts = time.time_ns()
 
-sls.trigger()
+    with open(f'{sls.pending}/gpu/{ts}.log', 'w') as f:
+        pid = None
+        for key, val in os.environ.items():
+            if key in ('PWD', '_'):
+                continue
+            if key == 'PID':
+                pid = int(val)
+            print(f'{key}={val}', file=f)
+        if pid:
+            appid = sls.util.get_appid(pid)
+            print(f'APPID={appid}', file=f)
+        print(f'TIMESTAMP={ts}', file=f)
+
+    sls.trigger()
+except Exception as e:
+    logger.critical('Unhandled exception', exc_info=e)
