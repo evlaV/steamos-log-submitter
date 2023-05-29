677ed55f68cbc0623817371e2353b7a14a10e253
Vicki Pfau <vi@endrift.com>
2023-05-29 15:42:41 -0700

    Add preliminary CLI tool

---
+++ steamos_log_submitter/cli.py
@@ -0,0 +1,50 @@
+import argparse
+import configparser
+import sys
+import steamos_log_submitter as sls
+import steamos_log_submitter.config as config
+
+
+def set_enabled(enable) -> bool:
+    if not config.user_config_path:
+        print("No user configuration file path found")
+        return False
+    user_config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
+    try:
+        with open(config.user_config_path) as f:
+            user_config.read_file(f, source=config.user_config_path)
+    except FileNotFoundError:
+        pass
+    except OSError:
+        print("Couldn't open configuration file")
+        return False
+
+    if not user_config.has_section('sls'):
+        user_config.add_section('sls')
+    user_config.set('sls', 'enable', 'on' if enable else 'off')
+
+    try:
+        with open(config.user_config_path, 'w') as f:
+            user_config.write(f)
+    except OSError:
+        print("Couldn't open configuration file")
+        return False
+    return True
+
+
+def main(args=sys.argv[1:]):
+    parser = argparse.ArgumentParser(
+        prog='steamos-log-submitter',
+        description='SteamOS log collectin and submission tool')
+    parser.add_argument('command', choices=('status', 'enable', 'disable'))
+    args = parser.parse_args(args)
+    if args.command == 'status':
+        print('Log submission is currently ' + ('enabled' if sls.base_config['enable'] == 'on' else 'disabled'))
+    if args.command == 'enable':
+        set_enabled(True)
+    if args.command == 'disable':
+        set_enabled(False)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_cli.py
@@ -0,0 +1,69 @@
+import os
+import tempfile
+import time
+import steamos_log_submitter.cli as cli
+import steamos_log_submitter.config as config
+from . import mock_config  # NOQA: F401
+
+
+def test_status(capsys, mock_config):
+    mock_config.add_section('sls')
+
+    mock_config.set('sls', 'enable', 'off')
+    cli.main(['status'])
+    assert capsys.readouterr().out.strip().endswith('disabled')
+
+    mock_config.set('sls', 'enable', 'on')
+    cli.main(['status'])
+    assert capsys.readouterr().out.strip().endswith('enabled')
+
+    mock_config.set('sls', 'enable', 'foo')
+    cli.main(['status'])
+    assert capsys.readouterr().out.strip().endswith('disabled')
+
+
+def test_enable(monkeypatch):
+    user_config = tempfile.NamedTemporaryFile(suffix='.cfg', dir=os.getcwd())
+    monkeypatch.setattr(config, 'user_config_path', user_config.name)
+    assert cli.set_enabled(True)
+    with open(user_config.name) as f:
+        assert f.read() == '[sls]\nenable = on\n\n'
+
+    assert cli.set_enabled(False)
+    with open(user_config.name) as f:
+        assert f.read() == '[sls]\nenable = off\n\n'
+
+
+def test_disable(monkeypatch):
+    user_config = tempfile.NamedTemporaryFile(suffix='.cfg', dir=os.getcwd())
+    monkeypatch.setattr(config, 'user_config_path', user_config.name)
+    assert cli.set_enabled(False)
+    with open(user_config.name) as f:
+        assert f.read() == '[sls]\nenable = off\n\n'
+
+    assert cli.set_enabled(True)
+    with open(user_config.name) as f:
+        assert f.read() == '[sls]\nenable = on\n\n'
+
+
+def test_no_config(monkeypatch):
+    monkeypatch.setattr(config, 'user_config_path', None)
+    assert not cli.set_enabled(True)
+
+
+def test_create_file(monkeypatch):
+    prefix = int((time.time() % 1) * 0x400000)
+    user_config = f'{prefix:06x}.cfg'
+    assert not os.access(user_config, os.F_OK)
+    monkeypatch.setattr(config, 'user_config_path', user_config)
+    try:
+        assert cli.set_enabled(True)
+        with open(user_config) as f:
+            assert f.read() == '[sls]\nenable = on\n\n'
+    finally:
+        os.unlink(user_config)
+
+
+def test_inaccessible_config(monkeypatch):
+    monkeypatch.setattr(config, 'user_config_path', '/doesnotexist')
+    assert not cli.set_enabled(True)
