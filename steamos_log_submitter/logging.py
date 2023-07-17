22d34e1b986de9ad13a9249894d5a5505224c450
Vicki Pfau <vi@endrift.com>
2023-07-17 16:22:03 -0700

    logging: Add rotation and logging to file/stream separation

--- steamos_log_submitter/logging.py
+++ steamos_log_submitter/logging.py
@@ -4,6 +4,7 @@
 # Copyright (c) 2022 Valve Software
 # Maintainer: Vicki Pfau <vi@endrift.com>
 import logging
+import logging.handlers
 import steamos_log_submitter as sls
 
 __all__ = [
@@ -12,33 +13,32 @@ __all__ = [
 
 config = sls.get_config(__name__)
 logger = logging.getLogger(__name__)
+root_logger = logging.getLogger()
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s: %(message)s')
 
 
-def reconfigure_logging(path=None):
+def add_handler(handler: logging.Handler):
     level = config.get('level', 'WARNING').upper()
     try:
         level = getattr(logging, level)
     except AttributeError:
         level = logging.WARNING
 
-    kwargs = {
-        'encoding': 'utf-8',
-        'level': level,
-        'format': '%(asctime)s %(levelname)s %(name)s: %(message)s',
-        'force': True
-    }
+    handler.setFormatter(formatter)
+    handler.setLevel(level)
+    root_logger.addHandler(handler)
+
+
+def reconfigure_logging(path: str = None):
+    for handler in list(root_logger.handlers):
+        root_logger.removeHandler(handler)
+        handler.close()
+    add_handler(logging.StreamHandler())
 
     if path is None:
         path = config.get('path')
     if path:
-        kwargs['filename'] = path
-
-    try:
-        logging.basicConfig(**kwargs)
-    except OSError:
-        # If we got an error trying to open the log file, retry without the file
-        if not path:
-            raise
-        del kwargs['filename']
-        logging.basicConfig(**kwargs)
-        logger.warning("Couldn't open log file")
+        try:
+            add_handler(logging.handlers.TimedRotatingFileHandler(path, when='midnight', interval=7, backupCount=4, encoding='utf-8'))
+        except OSError:
+            logger.warning("Couldn't open log file")
