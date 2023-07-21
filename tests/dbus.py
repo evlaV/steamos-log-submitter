55dd6c3c14c07ea1830b0bf8eca1d34ab623dab1
Vicki Pfau <vi@endrift.com>
2023-07-21 16:43:21 -0700

    dbus: Migrate to dbus_next for async

--- tests/dbus.py
+++ tests/dbus.py
@@ -3,21 +3,26 @@
 #
 # Copyright (c) 2022 Valve Software
 # Maintainer: Vicki Pfau <vi@endrift.com>
-import dbus
-import xml.etree.ElementTree as et
+import dbus_next
 import pytest
+import steamos_log_submitter as sls
+import steamos_log_submitter.dbus
+import xml.etree.ElementTree as et
 
 
 class MockDBusBus:
     def __init__(self):
         self.buses = {}
 
+    async def connect(self):
+        pass
+
     def get_object(self, bus_name, object_path):
         try:
             bus = self.buses[bus_name]
             return bus['path'][object_path]
         except KeyError:
-            raise MockDBusException('org.freedesktop.DBus.Error.ServiceUnknown')
+            raise dbus_next.errors.DBusError('org.freedesktop.DBus.Error.ServiceUnknown', '')
 
     def add_bus(self, bus_name):
         if bus_name not in self.buses:
@@ -39,21 +44,15 @@ class MockDBusBus:
                 bus['children'][split[0]] = {}
             parent = bus['children'][split[0]]
             parent[split[1]] = obj
+            bus['path'][split[0]].child_paths.append(object_path)
 
-
-class MockDBusObject:
-    def __init__(self, bus_name, object_path, bus):
-        self.bus_name = bus_name
-        self.object_path = object_path
-        self.bus = bus
-        bus.add_object(bus_name, object_path, self)
-
-        self.properties = {}
-
-    def Introspect(self):
+    async def introspect(self, bus_name, object_path):
         root = et.Element('node')
 
-        children = self.bus.buses[self.bus_name]['children'].get(self.object_path, {})
+        try:
+            children = self.buses[bus_name]['children'].get(object_path, {})
+        except KeyError:
+            raise dbus_next.errors.DBusError('org.freedesktop.DBus.Error.ServiceUnknown', '')
         for name in children.keys():
             node = et.Element('node')
             node.attrib['name'] = name
@@ -61,39 +60,86 @@ class MockDBusObject:
 
         return et.tostring(root)
 
+    def get_proxy_object(self, bus_name, object_path, introspection):
+        return self.buses[bus_name]['path'][object_path]
 
-class MockDBusPropertiesInterface:
-    @staticmethod
-    def Get(obj, iface, prop):
+
+class MockDBusProperties:
+    def __init__(self, obj, iface):
         try:
-            return obj.properties[iface][prop]
-        except KeyError as e:
-            raise MockDBusException('org.freedesktop.DBus.Error.InvalidArgs') from e
+            self.properties = obj.properties[iface]
+        except KeyError:
+            raise dbus_next.errors.InterfaceNotFoundError(iface)
+        self._iface = iface
+
+    async def __getitem__(self, name):
+        if name not in self.properties:
+            raise AttributeError(name)
+
+
+class MockDBusObject:
+    def __init__(self, bus_name, object_path, bus):
+        self.bus_name = bus_name
+        self.object_path = object_path
+        self.bus = bus
+        self.child_paths = []
+        bus.add_object(bus_name, object_path, self)
+
+        self.properties = {}
+        self.interfaces = {'org.freedesktop.DBus.Properties': MockDBusPropertiesInterface(self)}
+
+    def get_interface(self, iface):
+        return self.interfaces[iface]
 
 
 class MockDBusInterface:
-    INTERFACES = {'org.freedesktop.DBus.Properties': MockDBusPropertiesInterface}
+    methods = {}
 
     def __init__(self, obj, iface):
         self.iface = iface
         self.object = obj
 
+    @classmethod
+    def method(cls, fn):
+        name = dbus_next.proxy_object.BaseProxyInterface._to_snake_case(fn.__name__)
+        cls.methods[name] = fn
+        return fn
+
     def __getattr__(self, attr):
-        return lambda *args, **kwargs: getattr(self.INTERFACES[self.iface], attr)(self.object, *args, **kwargs)
+        type, name = attr.split('_', 1)
+
+        async def call(*args, **kwargs):
+            method = self.methods[name]
+            return method(self, *args, **kwargs)
+
+        async def get():
+            properties_iface = self.object.interfaces['org.freedesktop.DBus.Properties']
+            return properties_iface.get(self.iface, name)
+
+        if type == 'call':
+            return call
+        if type == 'get':
+            return get
 
 
-class MockDBusException(dbus.exceptions.DBusException):
-    def __init__(self, name):
-        self.name = name
+class MockDBusPropertiesInterface(MockDBusInterface):
+    def __init__(self, obj):
+        super().__init__(obj, 'org.freedesktop.DBus.Properties')
+        self.methods['Get'] = self.get
 
-    def get_dbus_name(self):
-        return self.name
+    @MockDBusInterface.method
+    def get(self, iface, name):
+        return MockDBusVariant(self.object.properties[iface][name])
+
+
+class MockDBusVariant:
+    def __init__(self, value):
+        self.value = value
 
 
 @pytest.fixture
 def mock_dbus(monkeypatch):
     bus = MockDBusBus()
-    monkeypatch.setattr(dbus, 'SystemBus', lambda: bus)
-    monkeypatch.setattr(dbus, 'Interface', MockDBusInterface)
-
+    monkeypatch.setattr(dbus_next.aio, 'MessageBus', lambda bus_type: bus)
+    monkeypatch.setattr(sls.dbus, 'system_bus', bus)
     return bus
--- tests/test_dbus.py
+++ tests/test_dbus.py
@@ -1,57 +1,66 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 # vim:ts=4:sw=4:et
 #
-# Copyright (c) 2022 Valve Software
+# Copyright (c) 2022-2023 Valve Software
 # Maintainer: Vicki Pfau <vi@endrift.com>
+import dbus_next
+import pytest
 import steamos_log_submitter as sls
 import steamos_log_submitter.dbus
 from .dbus import mock_dbus, MockDBusObject  # NOQA: F401
-import dbus
 
 
-def test_invalid_object(mock_dbus):
+@pytest.mark.asyncio
+async def test_invalid_object(mock_dbus):
+    await sls.dbus.connect()
+    obj = sls.dbus.DBusObject('org.none', '/')
     try:
-        sls.dbus.DBusObject('org.none', '/', mock_dbus)
+        await obj._connect()
         assert False
-    except dbus.exceptions.DBusException as e:
-        assert e.get_dbus_name() == 'org.freedesktop.DBus.Error.ServiceUnknown'
+    except dbus_next.errors.DBusError as e:
+        assert e.type == 'org.freedesktop.DBus.Error.ServiceUnknown'
 
 
-def test_valid_object(mock_dbus):
+@pytest.mark.asyncio
+async def test_valid_object(mock_dbus):
     MockDBusObject('com.valvesoftware', '/', mock_dbus)
-    sls.dbus.DBusObject('com.valvesoftware', '/', mock_dbus)
+    obj = sls.dbus.DBusObject('com.valvesoftware', '/')
+    await obj._connect()
 
 
-def test_children_object(mock_dbus):
+@pytest.mark.asyncio
+async def test_children_object(mock_dbus):
     MockDBusObject('com.valvesoftware', '/a', mock_dbus)
     MockDBusObject('com.valvesoftware', '/a/b', mock_dbus)
-    obj = sls.dbus.DBusObject('com.valvesoftware', '/a', mock_dbus)
-    assert obj.list_children() == ['/a/b']
+    obj = sls.dbus.DBusObject('com.valvesoftware', '/a')
+    assert await obj.list_children() == ['/a/b']
 
 
-def test_object_properties(mock_dbus):
+@pytest.mark.asyncio
+async def test_object_properties(mock_dbus):
     mock_obj = MockDBusObject('com.valvesoftware', '/gordon', mock_dbus)
     mock_obj.properties['com.valvesoftware.Props'] = {
         'Name': 'Gordon Freeman',
         'Passport': False
     }
-    obj = sls.dbus.DBusObject('com.valvesoftware', '/gordon', mock_dbus)
+    obj = sls.dbus.DBusObject('com.valvesoftware', '/gordon')
     props = obj.properties('com.valvesoftware.Props')
-    assert props['Name'] == 'Gordon Freeman'
-    assert not props['Passport']
+    assert await props['Name'] == 'Gordon Freeman'
+    assert not await props['Passport']
     try:
-        assert not props['G-Man']
+        assert not await props['G-Man']
     except KeyError:
         pass
 
 
-def test_no_recursive_children_object(mock_dbus):
+@pytest.mark.asyncio
+async def test_no_recursive_children_object(mock_dbus):
     MockDBusObject('com.valvesoftware', '/a', mock_dbus)
     MockDBusObject('com.valvesoftware', '/a/b', mock_dbus)
     MockDBusObject('com.valvesoftware', '/a/b/c', mock_dbus)
-    obj = sls.dbus.DBusObject('com.valvesoftware', '/a', mock_dbus)
-    assert obj.list_children() == ['/a/b']
-    obj = sls.dbus.DBusObject('com.valvesoftware', '/a/b', mock_dbus)
-    assert obj.list_children() == ['/a/b/c']
-    obj = sls.dbus.DBusObject('com.valvesoftware', '/a/b/c', mock_dbus)
-    assert obj.list_children() == []
+    obj = sls.dbus.DBusObject('com.valvesoftware', '/a')
+    assert await obj.list_children() == ['/a/b']
+    obj = sls.dbus.DBusObject('com.valvesoftware', '/a/b')
+    assert await obj.list_children() == ['/a/b/c']
+    obj = sls.dbus.DBusObject('com.valvesoftware', '/a/b/c')
+    assert await obj.list_children() == []
