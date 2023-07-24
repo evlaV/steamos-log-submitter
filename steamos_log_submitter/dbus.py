5d96358ce722f28d20b5f8ebd96b044ce14f3f6d
Vicki Pfau <vi@endrift.com>
2023-07-24 12:11:29 -0700

    dbus: Add interface API

--- steamos_log_submitter/dbus.py
+++ steamos_log_submitter/dbus.py
@@ -3,13 +3,14 @@
 #
 # Copyright (c) 2022 Valve Software
 # Maintainer: Vicki Pfau <vi@endrift.com>
+import asyncio
 import dbus_next
 
 connected = False
 system_bus = None
 
 
-async def connect():
+async def connect():  # pragma: no cover
     global system_bus
     global connected
     if connected:
@@ -21,22 +22,69 @@ async def connect():
     connected = True
 
 
+class DBusInterface:
+    def __init__(self, obj, iface):
+        self._obj = obj
+        self._iface = iface
+        self._iface_handle = None
+
+    def __getattr__(self, name):
+        async def call(*args, **kwargs):
+            if not self._iface_handle:
+                if not self._obj.object:
+                    await self._obj._connect()
+                self._iface_handle = self._obj.object.get_interface(self._iface)
+            method = getattr(self._iface_handle, f'call_{name}')
+            setattr(self, name, method)  # Memoize the method so we only have to look it up once
+            return await method(*args, **kwargs)
+        return call
+
+
 class DBusProperties:
     def __init__(self, obj, iface):
         self._properties_iface = None
-        self.obj = obj
-        self.iface = iface
+        self._obj = obj
+        self._iface = iface
+        self._subscribed = {}
 
     async def __getitem__(self, name):
         if not self._properties_iface:
-            if not self.obj.object:
-                await self.obj._connect()
-            self._properties_iface = self.obj.object.get_interface('org.freedesktop.DBus.Properties')
+            if not self._obj.object:
+                await self._obj._connect()
+            self._properties_iface = self._obj.object.get_interface('org.freedesktop.DBus.Properties')
         try:
-            variant = await self._properties_iface.call_get(self.iface, name)
+            variant = await self._properties_iface.call_get(self._iface, name)
             return variant.value
         except OSError:
             raise
+
+    def _update_props(self, iface: str, changed: dict[str, dbus_next.Variant], invalidated: list[str]):
+        async def do_cb(cb, iface: str, prop: str, value):
+            if value is None:
+                value = await self._properties_iface.call_get(self._iface, prop)
+            else:
+                value = value.value
+            await cb(iface, prop, value)
+
+        for prop, value in changed.items():
+            if prop not in self._subscribed:
+                continue
+            for handler in self._subscribed[prop]:
+                asyncio.create_task(do_cb(handler, iface, prop, value))
+        for prop in invalidated:
+            if prop not in self._subscribed:
+                continue
+            for handler in self._subscribed[prop]:
+                asyncio.create_task(do_cb(handler, iface, prop, None))
+
+    async def subscribe(self, prop: str, cb):
+        if not self._obj.object:
+            await self._obj._connect()
+        if not self._subscribed:
+            iface_handle = self._obj.object.get_interface('org.freedesktop.DBus.Properties')
+            iface_handle.on_properties_changed(self._update_props)
+        self._subscribed[prop] = self._subscribed.get(prop, [])
+        self._subscribed[prop].append(cb)
 
 
 class DBusObject:
@@ -58,6 +106,16 @@ class DBusObject:
 
     def properties(self, iface: str) -> DBusProperties:
         return DBusProperties(self, iface)
+
+    async def interface(self, iface: str) -> DBusInterface:
+        await self._connect()
+        return DBusInterface(self, iface)
+
+    async def subscribe(self, iface: str, signal: str, cb):
+        await self._connect()
+        iface_handle = self.object.get_interface(iface)
+        name = dbus_next.proxy_object.BaseProxyInterface._to_snake_case(signal)
+        getattr(iface_handle, f'on_{name}')(cb)
 
     async def list_children(self):
         await self._connect()
--- tests/test_dbus.py
+++ tests/test_dbus.py
@@ -3,11 +3,13 @@
 #
 # Copyright (c) 2022-2023 Valve Software
 # Maintainer: Vicki Pfau <vi@endrift.com>
+import asyncio
 import dbus_next
 import pytest
 import steamos_log_submitter as sls
 import steamos_log_submitter.dbus
-from .dbus import mock_dbus, MockDBusObject  # NOQA: F401
+from . import count_hits  # NOQA: F401
+from .dbus import mock_dbus, MockDBusInterface, MockDBusObject, MockDBusProperties  # NOQA: F401
 
 
 @pytest.mark.asyncio
@@ -64,3 +66,51 @@ async def test_no_recursive_children_object(mock_dbus):
     assert await obj.list_children() == ['/a/b/c']
     obj = sls.dbus.DBusObject('com.valvesoftware', '/a/b/c')
     assert await obj.list_children() == []
+
+
+@pytest.mark.asyncio
+async def test_interface(mock_dbus, count_hits):
+    mock_obj = MockDBusObject('com.valvesoftware', '/crowbar', mock_dbus)
+
+    class TestDBusInterface(MockDBusInterface):
+        def __init__(self, obj):
+            super().__init__(self, 'com.valvesoftware.Crowbar')
+            self._methods['swing'] = self.swing
+
+        def swing(self):
+            count_hits()
+
+    mock_iface = TestDBusInterface(mock_obj)
+    mock_obj.add_interface(mock_iface)
+
+    obj = sls.dbus.DBusObject('com.valvesoftware', '/crowbar')
+    iface = await obj.interface('com.valvesoftware.Crowbar')
+    print(iface)
+    await iface.swing()
+    assert count_hits.hits == 1
+
+
+@pytest.mark.asyncio
+async def test_property_signaling(mock_dbus):
+    mock_obj = MockDBusObject('com.valvesoftware', '/charging', mock_dbus)
+    mock_obj.properties['com.valvesoftware.Props'] = {
+        'Value': 0,
+    }
+    mock_props = MockDBusProperties(mock_obj, 'com.valvesoftware.Props')
+    obj = sls.dbus.DBusObject('com.valvesoftware', '/charging')
+    props = obj.properties('com.valvesoftware.Props')
+    assert await props['Value'] == 0
+    assert await mock_props['Value'] == 0
+    hit = False
+
+    async def test_full(iface, prop, value):
+        nonlocal hit
+        assert iface == 'com.valvesoftware.Props'
+        assert prop == 'Value'
+        assert value == 1
+        hit = True
+
+    await props.subscribe('Value', test_full)
+    mock_props['Value'] = 1
+    await asyncio.sleep(0)
+    assert hit
