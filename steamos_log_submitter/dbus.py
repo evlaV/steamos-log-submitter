cef81892fa0d6f7e736c5a7aeba50946f2443996
Vicki Pfau <vi@endrift.com>
2023-07-26 15:50:06 -0700

    dbus: Improve type annotations

--- steamos_log_submitter/dbus.py
+++ steamos_log_submitter/dbus.py
@@ -5,12 +5,13 @@
 # Maintainer: Vicki Pfau <vi@endrift.com>
 import asyncio
 import dbus_next
+from typing import Any, Awaitable, Callable, Iterable, Optional
 
 connected = False
 system_bus = None
 
 
-async def connect():  # pragma: no cover
+async def connect() -> None:  # pragma: no cover
     global system_bus
     global connected
     if connected:
@@ -23,16 +24,15 @@ async def connect():  # pragma: no cover
 
 
 class DBusInterface:
-    def __init__(self, obj, iface):
+    def __init__(self, obj: 'DBusObject', iface: str):
         self._obj = obj
         self._iface = iface
-        self._iface_handle = None
+        self._iface_handle: Optional[dbus_next.aio.ProxyInterface] = None
 
-    def __getattr__(self, name):
-        async def call(*args, **kwargs):
+    def __getattr__(self, name: str) -> Callable:
+        async def call(*args: Any, **kwargs: Any) -> Any:
             if not self._iface_handle:
-                if not self._obj.object:
-                    await self._obj._connect()
+                await self._obj._connect()
                 self._iface_handle = self._obj.object.get_interface(self._iface)
             method = getattr(self._iface_handle, f'call_{name}')
             setattr(self, name, method)  # Memoize the method so we only have to look it up once
@@ -41,27 +41,26 @@ class DBusInterface:
 
 
 class DBusProperties:
-    def __init__(self, obj, iface):
-        self._properties_iface = None
+    def __init__(self, obj: 'DBusObject', iface: str):
+        self._properties_iface: Optional[dbus_next.aio.ProxyInterface] = None
         self._obj = obj
         self._iface = iface
-        self._subscribed = {}
+        self._subscribed: dict[str, list[Callable[[str, str, Any], Awaitable[None]]]] = {}
 
-    async def __getitem__(self, name):
+    async def __getitem__(self, name: str) -> Any:
         if not self._properties_iface:
-            if not self._obj.object:
-                await self._obj._connect()
+            await self._obj._connect()
             self._properties_iface = self._obj.object.get_interface('org.freedesktop.DBus.Properties')
         try:
-            variant = await self._properties_iface.call_get(self._iface, name)
+            variant = await self._properties_iface.call_get(self._iface, name)  # type: ignore
             return variant.value
         except OSError:
             raise
 
-    def _update_props(self, iface: str, changed: dict[str, dbus_next.Variant], invalidated: list[str]):
-        async def do_cb(cb, iface: str, prop: str, value):
+    def _update_props(self, iface: str, changed: dict[str, dbus_next.Variant], invalidated: list[str]) -> None:
+        async def do_cb(cb: Callable[[str, str, Any], Awaitable[None]], iface: str, prop: str, value: Any) -> None:
             if value is None:
-                value = await self._properties_iface.call_get(self._iface, prop)
+                value = await self._properties_iface.call_get(self._iface, prop)  # type: ignore
             value = value.value
             await cb(iface, prop, value)
 
@@ -76,12 +75,11 @@ class DBusProperties:
             for handler in self._subscribed[prop]:
                 asyncio.create_task(do_cb(handler, iface, prop, None))
 
-    async def subscribe(self, prop: str, cb):
-        if not self._obj.object:
-            await self._obj._connect()
+    async def subscribe(self, prop: str, cb: Callable[[str, str, Any], Awaitable[None]]) -> None:
+        await self._obj._connect()
         if not self._subscribed:
             iface_handle = self._obj.object.get_interface('org.freedesktop.DBus.Properties')
-            iface_handle.on_properties_changed(self._update_props)
+            iface_handle.on_properties_changed(self._update_props)  # type: ignore
         self._subscribed[prop] = self._subscribed.get(prop, [])
         self._subscribed[prop].append(cb)
 
@@ -91,15 +89,16 @@ class DBusObject:
         self.bus = system_bus
         self.bus_name = bus_name
         self.object_path = object_path
-        self.object = None
+        self.connected = False
 
-    async def _connect(self):
-        if self.object:
+    async def _connect(self) -> None:
+        if self.connected:
             return
         if not connected:
             await connect()
         if not self.bus:
             self.bus = system_bus
+        assert self.bus
         introspection = await self.bus.introspect(self.bus_name, self.object_path)
         self.object = self.bus.get_proxy_object(self.bus_name, self.object_path, introspection)
 
@@ -110,12 +109,12 @@ class DBusObject:
         await self._connect()
         return DBusInterface(self, iface)
 
-    async def subscribe(self, iface: str, signal: str, cb):
+    async def subscribe(self, iface: str, signal: str, cb: Callable[[str, str, Any], None]) -> None:
         await self._connect()
         iface_handle = self.object.get_interface(iface)
         name = dbus_next.proxy_object.BaseProxyInterface._to_snake_case(signal)
         getattr(iface_handle, f'on_{name}')(cb)
 
-    async def list_children(self):
+    async def list_children(self) -> Iterable[str]:
         await self._connect()
         return self.object.child_paths
