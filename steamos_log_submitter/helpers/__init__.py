cbceeedf0203b4087403dcc96bdfac541e089c3c
Vicki Pfau <vi@endrift.com>
2023-07-27 14:30:21 -0700

    helpers: Fix mypy errors

--- steamos_log_submitter/helpers/__init__.py
+++ steamos_log_submitter/helpers/__init__.py
@@ -9,7 +9,8 @@ import os
 import pkgutil
 import sys
 import tempfile
-from typing import Optional
+from types import TracebackType
+from typing import Any, Iterable, Optional, Type
 import steamos_log_submitter as sls
 import steamos_log_submitter.sentry
 import steamos_log_submitter.lockfile
@@ -22,12 +23,12 @@ class HelperResult:
     PERMANENT_ERROR = -2
     CLASS_ERROR = -3
 
-    def __init__(self, code=OK):
+    def __init__(self, code: int = OK):
         super(HelperResult, self).__init__()
         self.code = code
 
     @staticmethod
-    def check(result, *, true_code=OK, false_code=TRANSIENT_ERROR):
+    def check(result: bool, *, true_code: int = OK, false_code: int = TRANSIENT_ERROR) -> 'HelperResult':
         if result:
             return HelperResult(true_code)
         return HelperResult(false_code)
@@ -36,13 +37,17 @@ class HelperResult:
 class Helper:
     defaults: Optional[dict[str, Optional[str]]] = None
 
-    @classmethod
-    def __init_subclass__(cls):
-        cls.logger = logging.getLogger(cls.__module__)
-        sys.modules[cls.__module__].helper = cls
+    config: sls.config.ConfigSection
+    data: sls.data.DataStore
+    logger: logging.Logger
 
     @classmethod
-    def _setup(cls):
+    def __init_subclass__(cls) -> None:
+        cls.logger = logging.getLogger(cls.__module__)
+        sys.modules[cls.__module__].helper = cls  # type: ignore
+
+    @classmethod
+    def _setup(cls) -> None:
         cls.config = sls.config.get_config(cls.__module__)
         cls.data = sls.data.get_data(cls.__module__, defaults=cls.defaults)
 
@@ -57,12 +62,12 @@ class Helper:
 
 class SentryHelper(Helper):
     @classmethod
-    async def send_event(cls, **kwargs):
+    async def send_event(cls, **kwargs: Any) -> HelperResult:
         ok = await sls.sentry.send_event(cls.config['dsn'], **kwargs)
         return HelperResult.check(ok)
 
 
-def create_helper(category):
+def create_helper(category: str) -> Helper:
     try:
         helper = importlib.import_module(f'steamos_log_submitter.helpers.{category}')
         if not hasattr(helper, 'helper'):
@@ -73,29 +78,29 @@ def create_helper(category):
     return helper.helper
 
 
-def list_helpers():
-    return [helper.name for helper in pkgutil.iter_modules(__path__)]
+def list_helpers() -> Iterable[str]:
+    return (helper.name for helper in pkgutil.iter_modules(__path__))
 
 
-def lock(helper):
+def lock(helper: str) -> sls.lockfile.Lockfile:
     return sls.lockfile.Lockfile(f'{sls.pending}/{helper}/.lock')
 
 
 class StagingFile:
-    def __init__(self, category, name, mode='w+b'):
+    def __init__(self, category: str, name: str, mode: str = 'w+b'):
         self._final_name = f'{sls.pending}/{category}/{name}'
         self._tempfile = tempfile.NamedTemporaryFile(mode=mode, dir=f'{sls.pending}/{category}', prefix='.staging-', delete=False)
 
-    def close(self):
+    def close(self) -> None:
         self._tempfile.close()
         os.rename(self.name, self._final_name)
 
-    def __getattr__(self, attr):
+    def __getattr__(self, attr: str) -> Any:
         return getattr(self._tempfile, attr)
 
-    def __enter__(self):
+    def __enter__(self) -> 'StagingFile':
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: Optional[Type[BaseException]], exc_val: Optional[BaseException], exc_tb: Optional[TracebackType]) -> bool:
         self.close()
         return not exc_type
