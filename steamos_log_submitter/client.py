1a195b8607a8c810dfaa81ef3f66076ae1eb2afc
Vicki Pfau <vi@endrift.com>
2023-07-28 17:40:07 -0700

    client: Fix mypy errors

--- steamos_log_submitter/client.py
+++ steamos_log_submitter/client.py
@@ -8,13 +8,17 @@ import socket
 from typing import Any, Optional
 
 import steamos_log_submitter.daemon as daemon
+from steamos_log_submitter.types import JSON
 
 logger = logging.getLogger(__name__)
 
 
 class ClientError(RuntimeError):
-    def __init__(self, reply=None):
-        super().__init__(reply.data)
+    def __init__(self, reply: Optional[daemon.Reply] = None):
+        if reply:
+            super().__init__(reply.data)
+        else:
+            super().__init__()
         self.reply = reply
 
 
@@ -43,7 +47,7 @@ exception_map = {
 
 
 class Client:
-    def __init__(self, sock=None, *, path=None):
+    def __init__(self, sock: Optional[socket.socket] = None, *, path: Optional[str] = None):
         if sock:
             self._socket = sock
         else:
@@ -53,33 +57,35 @@ class Client:
     def _transact(self, command: str, args: Optional[dict[str, Any]] = None) -> Any:
         command_obj = daemon.Command(command, args)
         self._socket.send(command_obj.serialize())
-        reply = self._socket.recv(4096)
-        reply = daemon.Reply.deserialize(reply)
+        reply_bytes = self._socket.recv(4096)
+        reply = daemon.Reply.deserialize(reply_bytes)
+        if not reply:
+            raise UnknownError()
         if reply.status == daemon.Reply.OK:
             return reply.data
         exc = exception_map[reply.status]
         raise exc(reply)
 
-    def enable(self, state: bool = True):
+    def enable(self, state: bool = True) -> None:
         self._transact('enable', {'state': state})
 
-    def disable(self):
+    def disable(self) -> None:
         self.enable(False)
 
-    def enable_helpers(self, helpers: list[str]):
+    def enable_helpers(self, helpers: list[str]) -> None:
         self.set_helpers_enabled({helper: True for helper in helpers})
 
-    def disable_helpers(self, helpers: list[str]):
+    def disable_helpers(self, helpers: list[str]) -> None:
         self.set_helpers_enabled({helper: False for helper in helpers})
 
-    def set_helpers_enabled(self, helpers: dict[str, bool]):
+    def set_helpers_enabled(self, helpers: dict[str, bool]) -> None:
         self._transact('enable-helpers', {'helpers': helpers})
 
     def status(self) -> bool:
         reply = self._transact('status')
         return reply['enabled']
 
-    def helper_status(self, helpers: Optional[list[str]] = None) -> dict[str, dict[str, Any]]:
+    def helper_status(self, helpers: Optional[list[str]] = None) -> dict[str, dict[str, JSON]]:
         if helpers is None:
             reply = self._transact('helper-status')
         else:
@@ -92,14 +98,14 @@ class Client:
     def log_level(self) -> str:
         return self._transact('log-level')['level']
 
-    def set_log_level(self, level: str):
-        return self._transact('log-level', {'level': level})
+    def set_log_level(self, level: str) -> None:
+        self._transact('log-level', {'level': level})
 
-    def set_steam_info(self, key: str, value: Any):
+    def set_steam_info(self, key: str, value: Any) -> None:
         self._transact('set-steam-info', {'key': key, 'value': value})
 
-    def shutdown(self):
+    def shutdown(self) -> None:
         self._transact('shutdown')
 
-    def trigger(self, wait=True):
+    def trigger(self, wait: bool = True) -> None:
         self._transact('trigger', {'wait': wait})
