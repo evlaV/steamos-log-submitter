30fcb3c79cfed301dcec5aebb7c0b46e89ad150d
Vicki Pfau <vi@endrift.com>
2023-08-11 13:26:19 -0700

    run: Import psutil to ionice down process

--- steamos_log_submitter/run.py
+++ steamos_log_submitter/run.py
@@ -3,9 +3,16 @@
 #
 # Copyright (c) 2022-2023 Valve Software
 # Maintainer: Vicki Pfau <vi@endrift.com>
-import os
-
 if __name__ == '__main__':  # pragma: no cover
+    import logging
+    import os
+    import psutil
     from . import trigger
-    os.nice(19)
+
+    logger = logging.getLogger(__name__)
+    try:
+        os.nice(19)
+        psutil.Process().ionice(psutil.IOPRIO_CLASS_IDLE)
+    except OSError as e:
+        logger.warning('Failed to downgrade process priority', exc_info=e)
     trigger()
