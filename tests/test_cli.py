2f4746c3dc4de4c12a4cec84f6b71b6daa20cf30
Vicki Pfau <vi@endrift.com>
2023-08-11 15:59:44 -0700

    cli: Add missing license headers

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,8 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# vim:ts=4:sw=4:et
+#
+# Copyright (c) 2023 Valve Software
+# Maintainer: Vicki Pfau <vi@endrift.com>
 import pwd
 import steamos_log_submitter.cli as cli
 import steamos_log_submitter.daemon as daemon
