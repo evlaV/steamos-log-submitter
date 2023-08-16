3c60613ebaf91b5ae6a7f953c1740d1af3a85eb5
Vicki Pfau <vi@endrift.com>
2023-08-16 16:33:49 -0700

    tests/daemon: More D-Bus tests

--- tests/test_daemon_dbus.py
+++ tests/test_daemon_dbus.py
@@ -3,6 +3,7 @@
 #
 # Copyright (c) 2023 Valve Software
 # Maintainer: Vicki Pfau <vi@endrift.com>
+import dbus_next as dbus
 import pytest
 import steamos_log_submitter as sls
 import steamos_log_submitter.helpers
@@ -58,4 +59,88 @@ async def test_enabled(dbus_daemon, mock_config):
     assert await props['Enabled'] is True
     await props.set('Enabled', False)
     assert await props['Enabled'] is False
+    await daemon.shutdown()
+
+
+@pytest.mark.asyncio
+async def test_helper_enabled(dbus_daemon, helper_directory, mock_config, monkeypatch):
+    def list_helpers():
+        return ['test']
+
+    monkeypatch.setattr(sls.helpers, 'list_helpers', list_helpers)
+    daemon, bus = await dbus_daemon
+    manager = sls.dbus.DBusObject(bus, '/com/valvesoftware/SteamOSLogSubmitter/helpers/Test')
+    props = manager.properties('com.valvesoftware.SteamOSLogSubmitter.Helper')
+    assert await props['Enabled'] is True
+    await props.set('Enabled', False)
+    assert await props['Enabled'] is False
+    assert mock_config.get('helpers.test', 'enable') == 'off'
+    await daemon.shutdown()
+
+
+@pytest.mark.asyncio
+async def test_helper_collect_enabled(dbus_daemon, helper_directory, mock_config, monkeypatch):
+    def list_helpers():
+        return ['test']
+
+    monkeypatch.setattr(sls.helpers, 'list_helpers', list_helpers)
+    daemon, bus = await dbus_daemon
+    manager = sls.dbus.DBusObject(bus, '/com/valvesoftware/SteamOSLogSubmitter/helpers/Test')
+    props = manager.properties('com.valvesoftware.SteamOSLogSubmitter.Helper')
+    assert await props['CollectEnabled'] is True
+    await props.set('CollectEnabled', False)
+    assert await props['CollectEnabled'] is False
+    assert mock_config.get('helpers.test', 'collect') == 'off'
+    await daemon.shutdown()
+
+
+@pytest.mark.asyncio
+async def test_helper_submit_enabled(dbus_daemon, helper_directory, mock_config, monkeypatch):
+    def list_helpers():
+        return ['test']
+
+    monkeypatch.setattr(sls.helpers, 'list_helpers', list_helpers)
+    daemon, bus = await dbus_daemon
+    manager = sls.dbus.DBusObject(bus, '/com/valvesoftware/SteamOSLogSubmitter/helpers/Test')
+    props = manager.properties('com.valvesoftware.SteamOSLogSubmitter.Helper')
+    assert await props['SubmitEnabled'] is True
+    await props.set('SubmitEnabled', False)
+    assert await props['SubmitEnabled'] is False
+    assert mock_config.get('helpers.test', 'submit') == 'off'
+    await daemon.shutdown()
+
+
+@pytest.mark.asyncio
+async def test_inhibited(dbus_daemon, mock_config):
+    daemon, bus = await dbus_daemon
+    manager = sls.dbus.DBusObject(bus, '/com/valvesoftware/SteamOSLogSubmitter/Manager')
+    props = manager.properties('com.valvesoftware.SteamOSLogSubmitter.Manager')
+    assert await props['Inhibited'] is False
+    await daemon.inhibit(True)
+    assert await props['Inhibited'] is True
+    await props.set('Inhibited', False)
+    assert await props['Inhibited'] is False
+    await daemon.shutdown()
+
+
+@pytest.mark.asyncio
+async def test_set_steam_info(dbus_daemon, mock_config):
+    daemon, bus = await dbus_daemon
+    manager = sls.dbus.DBusObject(bus, '/com/valvesoftware/SteamOSLogSubmitter/Manager')
+    iface = await manager.interface('com.valvesoftware.SteamOSLogSubmitter.Manager')
+
+    await iface.set_steam_info('account_name', 'gaben')
+    assert sls.steam.get_steam_account_name() == 'gaben'
+
+    await iface.set_steam_info('account_id', '12345')
+    assert sls.steam.get_steam_account_id() == 12345
+
+    await iface.set_steam_info('deck_serial', 'HEV Mark IV')
+    assert sls.steam.get_deck_serial() == 'HEV Mark IV'
+
+    try:
+        await iface.set_steam_info('invalid', 'foo')
+        assert False
+    except dbus.errors.DBusError as e:
+        assert e.type == 'org.freedesktop.DBus.Error.InvalidArgs'
     await daemon.shutdown()
