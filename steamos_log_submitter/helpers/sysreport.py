1314e71f7867705a989ac0889096715befeb9793
Vicki Pfau <vi@endrift.com>
2024-06-12 18:19:18 -0700

    helpers/sysreport: Start bringing up sysreport helper

---
+++ steamos_log_submitter/helpers/sysreport.py
@@ -0,0 +1,98 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# vim:ts=4:sw=4:et
+#
+# Copyright (c) 2024 Valve Software
+# Maintainer: Vicki Pfau <vi@endrift.com>
+import dbus_next as dbus
+import logging
+import os
+import random
+import shutil
+from . import Helper, HelperResult
+
+import steamos_log_submitter as sls
+from steamos_log_submitter.aggregators.sentry import SentryEvent
+from steamos_log_submitter.constants import DBUS_NAME
+from steamos_log_submitter.types import JSONEncodable
+
+logger = logging.getLogger(__name__)
+
+
+class SysreportHelper(Helper):
+    valid_extensions = frozenset({'.zip'})
+    alphabet = '34679ABEHJKLMNPSTUWXYZ'
+
+    @classmethod
+    def _setup(cls) -> None:
+        super()._setup()
+        cls.extra_ifaces.append(SysreportInterface())
+
+    @classmethod
+    def make_id(cls) -> str:
+        id = random.choices(cls.alphabet, k=8)
+        return ''.join(id[:4]) + '-' + ''.join(id[4:])
+
+    @classmethod
+    async def submit(cls, fname: str) -> HelperResult:
+        id, _ = os.path.splitext(os.path.basename(fname))
+
+        tags: dict[str, JSONEncodable] = {
+            'friendly_id': id
+        }
+
+        try:
+            with open(fname, 'rb') as f:
+                attachment = f.read()
+        except OSError:
+            return HelperResult.TRANSIENT_ERROR
+
+        event = SentryEvent(cls.config['dsn'])
+        event.add_attachment({
+            'mime-type': 'application/zip',
+            'filename': 'report.zip',
+            'data': attachment
+        })
+        event.tags = tags
+        event.message = f'System report {id}'
+        return HelperResult.check(await event.send())
+
+    @classmethod
+    async def send_report(cls, path: str) -> str | HelperResult:
+        if not path.endswith('.zip'):
+            return HelperResult.PERMANENT_ERROR
+
+        id = cls.make_id()
+        name = f'{id}.zip'
+        new_path = f'{sls.pending}/{cls.name}/{name}'
+        shutil.copyfile(path, new_path)
+
+        result = (await sls.runner.submit_category(cls, [name]))[name]
+        if isinstance(result, HelperResult) and result == HelperResult.OK:
+            return id
+        try:
+            os.replace(new_path, f'{sls.failed}/{cls.name}/{id}.zip')
+        except OSError:
+            pass
+
+        if isinstance(result, Exception):
+            raise result
+        return result
+
+
+class SysreportInterface(dbus.service.ServiceInterface):
+    def __init__(self) -> None:
+        super().__init__(f'{DBUS_NAME}.Sysreport')
+
+    @dbus.service.method()
+    async def SendReport(self, path: 's') -> 's':  # type: ignore[name-defined] # NOQA: F821
+        try:
+            result = await SysreportHelper.send_report(path)
+        except FileNotFoundError as e:
+            raise dbus.errors.DBusError('org.freedesktop.DBus.Error.FileNotFound',
+                                        f'{e.strerror}: {e.filename}') from e
+        except Exception as e:
+            raise dbus.errors.DBusError('org.freedesktop.DBus.Error.Failed',
+                                        f'{type(e).__name__}: {str(e)}') from e
+        if isinstance(result, str):
+            return result
+        sls.helpers.raise_dbus_error(result)
---
+++ tests/helpers/test_sysreport.py
@@ -0,0 +1,168 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# vim:ts=4:sw=4:et
+#
+# Copyright (c) 2024 Valve Software
+# Maintainer: Vicki Pfau <vi@endrift.com>
+import dbus_next as dbus
+import pytest
+import os
+import tempfile
+import steamos_log_submitter as sls
+import steamos_log_submitter.aggregators.sentry as sentry
+from steamos_log_submitter.helpers import HelperResult
+from steamos_log_submitter.helpers.sysreport import SysreportHelper as helper
+from .. import awaitable, always_raise, custom_dsn, setup_categories
+from .. import helper_directory, mock_config, patch_module  # NOQA: F401
+from ..daemon import dbus_daemon  # NOQA: F401
+from ..dbus import mock_dbus, real_dbus, MockDBusObject  # NOQA: F401
+
+dsn = custom_dsn('helpers.sysreport')
+
+
+def test_id_format(monkeypatch):
+    monkeypatch.setattr(helper, 'alphabet', 'X')
+    assert helper.make_id() == 'XXXX-XXXX'
+
+
+@pytest.mark.asyncio
+async def test_send_failures(helper_directory):
+    assert await helper.send_report('report.tar.gz') == HelperResult.PERMANENT_ERROR
+    try:
+        await helper.send_report('/does/not/exist.zip')
+        assert False
+    except FileNotFoundError:
+        pass
+
+
+@pytest.mark.asyncio
+async def test_move_failed(helper_directory, monkeypatch):
+    monkeypatch.setattr(sls, 'base', helper_directory)
+    monkeypatch.setattr(helper, 'alphabet', 'X')
+    monkeypatch.setattr(helper, 'submit', awaitable(lambda _: HelperResult.TRANSIENT_ERROR))
+    setup_categories(['sysreport'])
+
+    zip = tempfile.NamedTemporaryFile(suffix='.zip')
+    assert await helper.send_report(zip.name) == HelperResult.TRANSIENT_ERROR
+    assert not os.access(f'{sls.pending}/{helper.name}/XXXX-XXXX.zip', os.F_OK)
+    assert os.access(f'{sls.failed}/{helper.name}/XXXX-XXXX.zip', os.F_OK)
+
+
+@pytest.mark.asyncio
+async def test_submit_missing_file(dbus_daemon):
+    daemon, bus = await dbus_daemon
+    usb = sls.dbus.DBusObject(bus, f'{sls.constants.DBUS_ROOT}/helpers/Sysreport')
+    iface = await usb.interface(f'{sls.constants.DBUS_NAME}.Sysreport')
+    try:
+        await iface.send_report('/does/not/exist.zip')
+        assert False
+    except dbus.errors.DBusError as e:
+        assert e.type == 'org.freedesktop.DBus.Error.FileNotFound'
+
+    await daemon.shutdown()
+
+
+@pytest.mark.asyncio
+async def test_transient_error(dbus_daemon, helper_directory, monkeypatch):
+    monkeypatch.setattr(sls, 'base', helper_directory)
+    monkeypatch.setattr(helper, 'alphabet', 'X')
+    monkeypatch.setattr(helper, 'submit', awaitable(lambda _: HelperResult.TRANSIENT_ERROR))
+    setup_categories(['sysreport'])
+
+    zip = tempfile.NamedTemporaryFile(suffix='.zip')
+
+    daemon, bus = await dbus_daemon
+    usb = sls.dbus.DBusObject(bus, f'{sls.constants.DBUS_ROOT}/helpers/Sysreport')
+    iface = await usb.interface(f'{sls.constants.DBUS_NAME}.Sysreport')
+
+    try:
+        await iface.send_report(zip.name)
+        assert False
+    except dbus.errors.DBusError as e:
+        assert e.type == f'{sls.constants.DBUS_NAME}.Error.TransientError'
+
+    assert not os.access(f'{sls.pending}/{helper.name}/XXXX-XXXX.zip', os.F_OK)
+    assert os.access(f'{sls.failed}/{helper.name}/XXXX-XXXX.zip', os.F_OK)
+
+
+@pytest.mark.asyncio
+async def test_permanent_error(dbus_daemon, helper_directory, monkeypatch):
+    monkeypatch.setattr(sls, 'base', helper_directory)
+    monkeypatch.setattr(helper, 'alphabet', 'X')
+    monkeypatch.setattr(helper, 'submit', awaitable(lambda _: HelperResult.PERMANENT_ERROR))
+    setup_categories(['sysreport'])
+
+    zip = tempfile.NamedTemporaryFile(suffix='.zip')
+
+    daemon, bus = await dbus_daemon
+    usb = sls.dbus.DBusObject(bus, f'{sls.constants.DBUS_ROOT}/helpers/Sysreport')
+    iface = await usb.interface(f'{sls.constants.DBUS_NAME}.Sysreport')
+
+    try:
+        await iface.send_report(zip.name)
+        assert False
+    except dbus.errors.DBusError as e:
+        assert e.type == f'{sls.constants.DBUS_NAME}.Error.PermanentError'
+
+    assert not os.access(f'{sls.pending}/{helper.name}/XXXX-XXXX.zip', os.F_OK)
+    assert os.access(f'{sls.failed}/{helper.name}/XXXX-XXXX.zip', os.F_OK)
+
+
+@pytest.mark.asyncio
+async def test_other_error(dbus_daemon, helper_directory, monkeypatch):
+    monkeypatch.setattr(sls, 'base', helper_directory)
+    monkeypatch.setattr(helper, 'alphabet', 'X')
+    monkeypatch.setattr(helper, 'submit', awaitable(always_raise(RuntimeError())))
+    setup_categories(['sysreport'])
+
+    zip = tempfile.NamedTemporaryFile(suffix='.zip')
+
+    daemon, bus = await dbus_daemon
+    usb = sls.dbus.DBusObject(bus, f'{sls.constants.DBUS_ROOT}/helpers/Sysreport')
+    iface = await usb.interface(f'{sls.constants.DBUS_NAME}.Sysreport')
+
+    try:
+        await iface.send_report(zip.name)
+        assert False
+    except dbus.errors.DBusError as e:
+        assert e.type == 'org.freedesktop.DBus.Error.Failed'
+
+    assert not os.access(f'{sls.pending}/{helper.name}/XXXX-XXXX.zip', os.F_OK)
+    assert os.access(f'{sls.failed}/{helper.name}/XXXX-XXXX.zip', os.F_OK)
+
+
+@pytest.mark.asyncio
+async def test_ok(dbus_daemon, helper_directory, monkeypatch):
+    monkeypatch.setattr(sls, 'base', helper_directory)
+    monkeypatch.setattr(helper, 'alphabet', 'X')
+    monkeypatch.setattr(helper, 'submit', awaitable(lambda _: HelperResult.OK))
+    setup_categories(['sysreport'])
+
+    zip = tempfile.NamedTemporaryFile(suffix='.zip')
+
+    daemon, bus = await dbus_daemon
+    usb = sls.dbus.DBusObject(bus, f'{sls.constants.DBUS_ROOT}/helpers/Sysreport')
+    iface = await usb.interface(f'{sls.constants.DBUS_NAME}.Sysreport')
+
+    assert await iface.send_report(zip.name) == 'XXXX-XXXX'
+    assert not os.access(f'{sls.pending}/{helper.name}/XXXX-XXXX.zip', os.F_OK)
+    assert os.access(f'{sls.uploaded}/{helper.name}/XXXX-XXXX.zip', os.F_OK)
+
+
+@pytest.mark.asyncio
+async def test_metadata(helper_directory, monkeypatch):
+    hit = False
+
+    async def check_now(self):
+        nonlocal hit
+        hit = True
+        assert self.tags['friendly_id'] == 'XXXX-XXXX'
+        assert self.message == 'System report XXXX-XXXX'
+        return True
+
+    monkeypatch.setattr(sentry.SentryEvent, 'send', check_now)
+    monkeypatch.setattr(sls, 'base', helper_directory)
+    monkeypatch.setattr(helper, 'alphabet', 'X')
+    setup_categories(['sysreport'])
+
+    zip = tempfile.NamedTemporaryFile(suffix='.zip')
+    assert await helper.send_report(zip.name) == 'XXXX-XXXX'
