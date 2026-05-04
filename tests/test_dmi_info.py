17724a6d08727cb8edfe4ccbc9047d32ae4b6082
Matthew Schwartz <matthew.schwartz@linux.dev>
2026-05-04 12:52:04 -0700

    tests: Add tests for new handhelds

--- tests/test_dmi_info.py
+++ tests/test_dmi_info.py
@@ -72,6 +72,69 @@ def test_rog_ally_x_sysinfo(monkeypatch, open_shim):
     }
 
 
+def test_rog_xbox_ally_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'ASUSTeK COMPUTER INC.\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'INVALID\n'
+        if fname.endswith('/board_name'):
+            return 'RC73YA\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'ASUSTeK COMPUTER INC.',
+        'product': 'ROG Xbox Ally',
+    }
+
+
+def test_rog_xbox_ally_x_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'ASUSTeK COMPUTER INC.\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'INVALID\n'
+        if fname.endswith('/board_name'):
+            return 'RC73XA\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'ASUSTeK COMPUTER INC.',
+        'product': 'ROG Xbox Ally X',
+    }
+
+
+def test_rog_flow_z13_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'ASUSTeK COMPUTER INC.\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'INVALID\n'
+        if fname.endswith('/board_name'):
+            return 'GZ302EA\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'ASUSTeK COMPUTER INC.',
+        'product': 'ROG Flow Z13 (2025)',
+    }
+
+
 def test_other_asus_sysinfo(monkeypatch, open_shim):
     def fake_dmi(fname):
         if fname.endswith('/sys_vendor'):
@@ -171,6 +234,27 @@ def test_zotac_zone_sysinfo(monkeypatch, open_shim):
     }
 
 
+def test_zotac_zone_g1a1w_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'ZOTAC\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'ZOTAC GAMING ZONE\n'
+        if fname.endswith('/board_name'):
+            return 'G1A1W\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'ZOTAC',
+        'product': 'ZONE',
+    }
+
+
 def test_other_zotac_sysinfo(monkeypatch, open_shim):
     def fake_dmi(fname):
         if fname.endswith('/sys_vendor'):
@@ -181,6 +265,456 @@ def test_other_zotac_sysinfo(monkeypatch, open_shim):
             return 'ZOTAC GAMING REGION\n'
         if fname.endswith('/board_name'):
             return 'G0Z1W\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert not info
+
+
+def test_anbernic_win600_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'Anbernic\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'Win600\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'Anbernic',
+        'product': 'Anbernic Win600',
+    }
+
+
+def test_other_anbernic_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'Anbernic\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'INVALID\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert not info
+
+
+def test_ayaneo_3_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'AYANEO\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'AYANEO 3\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'AYANEO',
+        'product': 'AYANEO 3',
+    }
+
+
+def test_ayaneo_air_1s_limited_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'AYANEO\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'AIR 1S Limited\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'AYANEO',
+        'product': 'AYANEO AIR 1S Limited',
+    }
+
+
+def test_ayaneo_flip_1s_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'AYANEO\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'FLIP 1S DS\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'AYANEO',
+        'product': 'AYANEO FLIP 1S',
+    }
+
+
+def test_gpd_win5_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'GPD\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'G1618-05\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'GPD',
+        'product': 'GPD WIN 5',
+    }
+
+
+def test_suiplay_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'MystenLabs, Inc.\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'SuiPlay 0X1\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'MystenLabs, Inc.',
+        'product': 'SuiPlay 0X1',
+    }
+
+
+def test_other_mystenlabs_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'MystenLabs, Inc.\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'INVALID\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert not info
+
+
+def test_onexplayer_old_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'ONE-NETBOOK\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'ONEXPLAYER\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'ONE-NETBOOK',
+        'product': 'ONEXPLAYER',
+    }
+
+
+def test_onexplayer_old_spaced_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'ONE-NETBOOK\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'ONE XPLAYER\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'ONE-NETBOOK',
+        'product': 'ONEXPLAYER',
+    }
+
+
+def test_onexplayer_old_alt_vendor_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'ONE-NETBOOK TECHNOLOGY CO., LTD.\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'ONE XPLAYER\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'ONE-NETBOOK TECHNOLOGY CO., LTD.',
+        'product': 'ONEXPLAYER',
+    }
+
+
+def test_onexplayer_mini_a07_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'ONE-NETBOOK\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'ONEXPLAYER mini A07\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'ONE-NETBOOK',
+        'product': 'ONEXPLAYER mini A07',
+    }
+
+
+def test_onexplayer_onexfly_eva_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'ONE-NETBOOK\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'ONEXPLAYER F1 EVA-01\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'ONE-NETBOOK',
+        'product': 'ONEXPLAYER OneXFly F1 EVA-01',
+    }
+
+
+def test_onexplayer_onexfly_oled_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'ONE-NETBOOK\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'ONEXPLAYER F1 OLED\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'ONE-NETBOOK',
+        'product': 'ONEXPLAYER OneXFly F1 OLED',
+    }
+
+
+def test_onexplayer_x1_amd_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'ONE-NETBOOK\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'ONEXPLAYER X1 A\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'ONE-NETBOOK',
+        'product': 'ONEXPLAYER X1 AMD',
+    }
+
+
+def test_onexplayer_x1_intel_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'ONE-NETBOOK\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'ONEXPLAYER X1 i\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'ONE-NETBOOK',
+        'product': 'ONEXPLAYER X1 Intel',
+    }
+
+
+def test_onexplayer_x1_mini_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'ONE-NETBOOK\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'ONEXPLAYER X1 mini\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'ONE-NETBOOK',
+        'product': 'ONEXPLAYER X1 mini',
+    }
+
+
+def test_onexplayer_g1_amd_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'ONE-NETBOOK\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'ONEXPLAYER G1 A\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'ONE-NETBOOK',
+        'product': 'ONEXPLAYER G1 AMD',
+    }
+
+
+def test_onexplayer_g1_intel_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'ONE-NETBOOK\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'ONEXPLAYER G1 i\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'ONE-NETBOOK',
+        'product': 'ONEXPLAYER G1 Intel',
+    }
+
+
+def test_other_one_netbook_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'ONE-NETBOOK\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'INVALID\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert not info
+
+
+def test_orangepi_neo_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'OrangePi\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'NEO-01\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
+        assert False
+
+    open_shim.cb(fake_dmi)
+
+    info = sls.util.get_dmi_info()
+    assert info == {
+        'vendor': 'OrangePi',
+        'product': 'OrangePi NEO',
+    }
+
+
+def test_other_orangepi_sysinfo(monkeypatch, open_shim):
+    def fake_dmi(fname):
+        if fname.endswith('/sys_vendor'):
+            return 'OrangePi\n'
+        if fname.endswith('/bios_version'):
+            return '2\n'
+        if fname.endswith('/product_name'):
+            return 'INVALID\n'
+        if fname.endswith('/board_name'):
+            return 'INVALID\n'
         assert False
 
     open_shim.cb(fake_dmi)
